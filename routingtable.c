4b328243ad5905e6f55b2e0a601fdb662ef24f0f
Alex <asymansk@purdue.edu>
2019-10-30 15:01:18 -0400

    routingtable funcitons pass unit tests

--- routingtable.c
+++ routingtable.c
@@ -8,16 +8,102 @@ int NumRoutes;
 
 
 ////////////////////////////////////////////////////////////////
-void InitRoutingTbl (struct pkt_INIT_RESPONSE *InitResponse, int myID){
-	/* ----- YOUR CODE HERE ----- */
+void InitRoutingTbl (struct pkt_INIT_RESPONSE *InitResponse, int myID)
+{
+	int i;
+	for(i = 0; i < (InitResponse->no_nbr); i++)
+	{
+		routingTable[i].next_hop = InitResponse->nbrcost[i].nbr;
+		routingTable[i].dest_id = InitResponse->nbrcost[i].nbr;
+		routingTable[i].cost = InitResponse->nbrcost[i].cost;
+		//need to add path variables
+		routingTable[i].path_len = 2; //router to neighbor
+		routingTable[i].path[0] = myID;
+		routingTable[i].path[1] = InitResponse->nbrcost[i].nbr ;
+	}
+
+	routingTable[i].next_hop = myID;
+	routingTable[i].dest_id = myID; 
+	routingTable[i].cost = 0;
+	routingTable[i].path_len = 1; //to self, one router in path
+	routingTable[i].path[0] = myID;
+
+	//update NumRoutes global variable
+	NumRoutes = (InitResponse->no_nbr) + 1;
+
 	return;
 }
 
 
 ////////////////////////////////////////////////////////////////
-int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myID){
-	/* ----- YOUR CODE HERE ----- */
-	return 0;
+int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myID)
+{
+	int i; //RecvdUpdatePacket->route iterator
+	int j; //routingTable iterator 
+	int k; //routingTable->path iterator 
+	int origRoutes = NumRoutes; //so tabel dosen't iterate through new entries
+	int tabelEdited = 0; //tracks if changes are made to routingTable
+	for(i = 0; i < RecvdUpdatePacket->no_routes; i++) //iterate through updated router table
+	{
+		struct route_entry entryUpdate = RecvdUpdatePacket->route[i];
+
+		int new = 1; //tracks if entry is new to routingTable 
+		for(j = 0; j < origRoutes; j++) //iterate through our current router tabel 
+		{
+			if(routingTable[j].dest_id == entryUpdate.dest_id)
+			{
+				new = 0;
+				if(entryUpdate.cost < routingTable[j].cost) //Path Vecor Rule
+				{ 	int ignore = 0;
+					for(k = 0; k < entryUpdate.path_len; k++) 
+					{
+						if(myID == entryUpdate.path[k]) //router found in path, ignore update
+						{
+							ignore = 1;
+							k = entryUpdate.path_len;
+						}
+					}
+					if(ignore != 1) //udate to better route
+					{
+						routingTable[j].next_hop = RecvdUpdatePacket->sender_id;
+						routingTable[j].cost = entryUpdate.cost + costToNbr;
+						routingTable[j].path_len = entryUpdate.path_len + 1;
+						routingTable[j].path[0] = myID;
+						for(k = 0; k < entryUpdate.path_len; k++)
+						{
+							routingTable[j].path[k+1] = entryUpdate.path[k];
+						}
+						tabelEdited = 1;
+					}
+				}
+				
+				if(routingTable[j].next_hop == RecvdUpdatePacket->sender_id) //forced update rule
+				{
+					if(entryUpdate.cost > routingTable[j].cost - costToNbr)
+					{
+						routingTable[j].cost = entryUpdate.cost + costToNbr; //updates to higher cost
+						tabelEdited = 1;
+					}
+				}
+			}
+		}
+
+		if(new == 1) //New router was found, adding it to routingTable
+		{
+			routingTable[NumRoutes].dest_id = entryUpdate.dest_id;
+			routingTable[NumRoutes].next_hop = RecvdUpdatePacket->sender_id;
+			routingTable[NumRoutes].cost = entryUpdate.cost + costToNbr;
+			routingTable[NumRoutes].path_len = entryUpdate.path_len + 1;
+			routingTable[NumRoutes].path[0] = myID;
+			for(k = 0; k < entryUpdate.path_len; k++)
+			{
+				routingTable[NumRoutes].path[k+1] = entryUpdate.path[k];
+			}
+			NumRoutes++;
+			tabelEdited = 1;
+		}
+	}
+	return tabelEdited;
 }
 
 
